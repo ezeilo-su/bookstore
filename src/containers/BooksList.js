0158f313b8d4a36f993d96a5b132d256a80b9857
ezeilo-su <ezeilo.su@gmail.com>
2021-04-06 17:36:51 +0100

    Implement the BooksList component

---
+++ src/containers/BooksList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
+
+export default function BooksList({ booksList, handleOnRemoveBook }) {
+  return (
+    <div>
+      <table style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th>Book Id</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {booksList.map((book) => (
+            <Book book={book} handleOnRemove={handleOnRemoveBook} key={`item${book + 1}`} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+BooksList.propTypes = {
+  booksList: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleOnRemoveBook: PropTypes.func.isRequired,
+};
