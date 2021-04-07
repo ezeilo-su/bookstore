eaba8afd88e683e43f96622ed11ee9a1884d1a1a
ezeilo-su <ezeilo.su@gmail.com>
2021-04-07 10:11:05 +0100

    Add more style to table columns

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,10 +5,10 @@ import '../styles/book.css';
 export default function Book({ book, onRemoveBook }) {
   return (
     <tr>
-      <td>{book.id}</td>
+      <td className="book-id">{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
-      <td>
+      <td className="remove-book">
         <button type="button" onClick={() => onRemoveBook(book)}>Remove Book</button>
       </td>
     </tr>
