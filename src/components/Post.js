5bc95bf04d2a34ec2cb6a47a74c7b25d10017e0b
ezeilo-su <ezeilo.su@gmail.com>
2021-04-10 12:16:31 +0100

    Add a subbmit button to read post id and fetch a single post

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,8 +4,9 @@ import axios from 'axios';
 export default function Post() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
   useEffect(() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((res) => {
         console.log(res);
         setPost(res.data);
@@ -16,12 +17,17 @@ export default function Post() {
     return () => {
       // cleanup
     };
-  }, [post]); // [] => dependency list - allows the callback fn passed
+  }, [idFromButtonClick]); // [] => dependency list - allows the callback fn passed
   // to useEffect to run only once on component mount
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
 
   return (
     <div>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>Fetch Post</button>
       <ul>
         <li>{post.title}</li>
       </ul>
